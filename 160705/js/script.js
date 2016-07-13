c14fa671da8b62f6267cc5e403a9185666192ea0
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-07-13 15:05:52 -0500

    building template for vertical scroll site

--- 160705/js/script.js
+++ 160705/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  
 
  $('.menu a').click(function(event){
   event.preventDefault();
@@ -6,6 +7,11 @@ $(document).ready(function() {
   $.smoothScroll({
     scrollTarget: link.hash
   });
+});
+
+ $('#name-link-0').mouseover('click touchstart', function() {
+    $('#bio0').animate({opacity: '1'}, 120); 
+     
 });
 
 
