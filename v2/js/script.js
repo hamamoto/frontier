3d67e6203293c3ed36b324567bcb76665398727b
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-08-11 14:28:17 -0500

    creating active menu

--- v2/js/script.js
+++ v2/js/script.js
@@ -10,16 +10,33 @@ $(document).ready(function() {
 	});
 
 	$('#name-link-0').mouseover('click touchstart', function() {
-    	$('#bio0').animate({opacity: '1'}, 120); 
-    });
+		$('#bio0').animate({opacity: '1'}, 120); 
+	});
+
+	$(window).scroll(function() {
+
+		var position = $(this).scrollTop();
+
+		$('.container').each(function() {
+			var target = $(this).offset().top;
+			var id = $(this).attr('id');
+			
+			if (position >= target) {
+				$('#menu > ul > li > a').removeClass('active');
+				$('#menu > ul > li > a[href=#' + id + ']').addClass('active');
+			}
+		});
+	});
 
 });
 
 $( window ).load(function() {
     // position page at center
  	var div = $('#frontier'); // take your div id
-    $(window).scrollTop(div.offset().top);
-	$('#allcontainer').removeClass('transparent');
-	$("#menu").removeClass("transparent");
+ 	$(window).scrollTop(div.offset().top);
+ 	$('#allcontainer').removeClass('transparent');
+ 	$("#menu").removeClass("transparent");
 
-});	
+ });	
+
+
