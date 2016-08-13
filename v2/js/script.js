4da7012fffaf490a3f5efd8a2cfa38da3a03c945
Chris Hamamoto <chamamoto@gmail.com>
2016-08-12 21:58:00 -0700

    random image placement and sizes, started mobile css

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,3 +1,17 @@
+// check viewport size
+
+function isMobile(){
+	return ($(window).width() <= 600 )
+}	
+
+function isTablet(){
+	return ($(window).width() <= 1024 && $(window).width() > 600 )
+}
+
+function isDesktop(){
+	return ($(window).width() > 1024)
+}
+
 $(document).ready(function() {
 
 
@@ -28,6 +42,42 @@ $(document).ready(function() {
 		});
 	});
 
+	// randomly apply image sizes and positions
+
+	// get random numbers, no repeats
+	var uniqueRandoms = [];
+	var numRandoms = 10;
+	function makeUniqueRandom() {
+	    // refill the array if needed
+	    if (!uniqueRandoms.length) {
+	        for (var i = 0; i < numRandoms; i++) {
+	            uniqueRandoms.push(i);
+	        }
+	    }
+	    var index = Math.floor(Math.random() * uniqueRandoms.length);
+	    var val = uniqueRandoms[index];
+
+	    // now remove that value from the array
+	    uniqueRandoms.splice(index, 1);
+
+	    return val;
+
+	}
+
+	// array for random sizes
+	var sizes = [1, 2, 3, 4];
+
+	// apply image and position randomly
+	$('.random').each(function() {
+  		$(this).addClass('image-pos-' + (makeUniqueRandom() + 1));
+		$(this).addClass('image-size-' + sizes[~~(Math.random()*sizes.length)]);
+	});	
+
+	if ( isMobile() ) {	
+	}	
+
+
+
 });
 
 $( window ).load(function() {
@@ -37,6 +87,13 @@ $( window ).load(function() {
  	$('#allcontainer').removeClass('transparent');
  	$("#menu").removeClass("transparent");
 
- });	
+
+ 	// show randomly placed images when window is loaded
+	$( '.random' ).each(function() {
+		$(this).addClass('show');
+	});	
+
+
+});	
 
 
